3b9905b156531b8aaf5813308291ca364cc373e7
Faiz <faiz9191@gmail.com>
2020-08-23 01:11:25 +0800

    Updates

---
+++ precache-manifest.c904df60a425163cef1ea83113abdd1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bb1ede9ecc2d9f7f40f070e962be636",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f6593c3f5f85911ef574",
+    "url": "/monster-rolodex/static/css/main.898390d1.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6593c3f5f85911ef574",
+    "url": "/monster-rolodex/static/js/main.97cddffa.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
